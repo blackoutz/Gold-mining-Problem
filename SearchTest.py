093a583b0c93feaeaf5367864b27f744204b0c18
Suchat Tungjitnob <suchat@muze.co.th>
2018-09-12 18:50:16 +0700

    TestShortestPath is working

--- SearchTest.py
+++ SearchTest.py
@@ -1,5 +1,5 @@
 from scipy.spatial import distance
-
+import heapq
 from collections import OrderedDict
 
 map = [[{'S':0},{'W':50},{'W':15},{'W':31},{'W':49},{'W':38},{'W':11},{'W':46},{'W':27},{'W':28},{'W':29},{'W':11},{'W':10},{'W':41},{'W':30},{'W':13}],
@@ -28,7 +28,11 @@ map = [[{'S':0},{'W':50},{'W':15},{'W':31},{'W':49},{'W':38},{'W':11},{'W':46},{
 [{'W':49},{'W':48},{'W':28},{'W':31},{'W':42},{'G':20103,'W':33},{'W':23},{'W':27},{'W':15},{'W':13},{'W':18},{'W':18},{'W':35},{'W':49},{'W':15},{'W':44}],
 [{'W':18},{'W':43},{'W':44},{'W':24},{'W':48},{'W':39},{'W':10},{'W':46},{'W':12},{'W':44},{'W':39},{'W':31},{'W':39},{'W':34},{'W':13},{'W':55}]]
 
-path = []
+
+START = (0,0)
+globalPath = []
+globalCost = []
+worthyMap = []
 allMine = []
 colMax = len(map[0])
 rowMax = len(map)
@@ -52,15 +56,22 @@ def running(row, col):
     if 'M' in map[row][col]:
         return None
     map[row][col]['M'] = 1
-    path.append([row, col])
+    #path.append([row, col])
 
     if 'G' in map[row][col] and not 'GM' in map[row][col]:
         mine = []
         findGold(row,col,mine)
-        # lastPath = []
-        # cost = 0
-        # newMap = map[:]
-       # shortestPath = findShortestPath(newMap,0,0,mine,lastPath,cost)
+        # minCost = 999999
+        # minPath = []
+        # for gold in mine:
+        #     newMap = map[:]
+        #     x,y = gold['pos']
+        #     path , cost = findShortestPath(newMap,x,y)
+        #     if cost < minCost:
+        #         minCost = cost
+        #         minPath = path
+        #
+        # worthyMap.append({'cost':minCost, 'path':minPath})
         allMine.append(mine)
 
     running(row+1, col)
@@ -79,7 +90,7 @@ def findClosetPoint(mine):
             closetPoint = [m['pos'][0], m['pos'][1]]
     return closetPoint
 
-def findShortestPath(newMap,row,col,mine,lastPath,cost):
+def findShortestPath(newMap,row,col):
 
     if col < 0:
         return None
@@ -93,8 +104,12 @@ def findShortestPath(newMap,row,col,mine,lastPath,cost):
     if 'SM' in newMap[row][col]:
         return None
     newMap[row][col]['SM'] = 1
-    
 
+    path,cost = ucs((0,0), (7,15), newMap)
+    print(cost)
+    return path,cost
+
+    '''
     directD = {}
     directD['up'] = {'row': row-1, 'col': col}
     directD['down'] = {'row': row+1, 'col': col}
@@ -118,7 +133,7 @@ def findShortestPath(newMap,row,col,mine,lastPath,cost):
     print(lastPath)
     print(Ncost+cost)
     findShortestPath(newMap,Nrow,Ncol,mine,lastPath,cost+Ncost)
-
+    '''
     #
     #
     # if min(up,down,left,right) == up:
@@ -129,6 +144,71 @@ def findShortestPath(newMap,row,col,mine,lastPath,cost):
     # if(min(map[row+1][col]))
 
 
+def children(point, newMap):
+    row, col = point
+
+    coordinate = {}
+
+    coordinate['up'] = newMap[row-1][col]['W'] if row-1 >= 0 and not 'SM' in newMap[row-1][col] and not 'R' in newMap[row-1][col] else 9999
+    coordinate['down'] = newMap[row+1][col]['W'] if row+1 < rowMax and not 'SM' in newMap[row+1][col] and not 'R' in newMap[row+1][col] else 9999
+    coordinate['left'] = newMap[row][col-1]['W'] if col-1 >= 0 and not 'SM' in newMap[row][col-1] and not 'R' in newMap[row][col-1] else 9999
+    coordinate['right'] = newMap[row][col+1]['W'] if col+1 < colMax and not 'SM' in newMap[row][col+1] and not 'R' in newMap[row][col+1] else 9999
+
+    children = [(row - 1, col), (row + 1, col),(row, col - 1), (row, col + 1) ]
+    #print(coordinate)
+    direction = []
+    for key,val in coordinate.items():
+        if(key == 'up' and val != 9999):
+            direction.append(children[0])
+        elif(key == 'down' and val != 9999):
+            direction.append(children[1])
+        elif(key == 'left' and val != 9999):
+            direction.append(children[2])
+        elif (key == 'right' and val != 9999):
+            direction.append(children[3])
+    #print(direction)
+    return direction
+
+    #
+    # for child in children:
+    #     for key,val in coordinate:
+    #         for
+    #         print('KEY ' ,key ,val)
+    #return [child for child in children if grid[child[0]][child[1]] != '%']
+
+    return coordinate
+
+
+
+def ucs(node, goal, grid):
+    # Initialize the queue with the root node
+    q = [(0, node, [])]
+    # The list of seen items
+    seen = {}
+    # While the queue isn't empty
+    while q:
+        # Pop the cost, point and path from the queue
+        cost, point, path = heapq.heappop(q)
+        # If it has been seen, and has a lower cost, bail
+        if point in seen and seen[point] < cost:
+            continue
+        # Update the path
+        path = path + [point]
+        # If we have found the goal, return the point
+        if point == goal:
+            return path,cost
+        # Loop through the children
+        for child in children(point, grid):
+            # Calculate the basic cost
+            x,y = child
+            child_cost = grid[x][y]['W']
+            # If the child hasn't been seen
+            if child not in seen:
+                # Add it to the heap
+                heapq.heappush(q, (cost + child_cost, child, path))
+        # Add the point to the seen items
+        seen[point] = cost
+    return None
 
 def findGold(row, col, mine):
     if col < 0:
@@ -155,12 +235,14 @@ def findGold(row, col, mine):
 
 
 running(0,0)
-print(allMine)
+#print(allMine)
+#print(worthyMap)
 
-lastPath = []
-cost = 0
+# lastPath = []
+# cost = 0
 newMap = map[:]
-shortestPath = findShortestPath(newMap,0,0,allMine[0],lastPath,cost)
+shortestPath,cost = findShortestPath(newMap,10,2)
+print(shortestPath)
 
-
+# children((0,2),map)
 

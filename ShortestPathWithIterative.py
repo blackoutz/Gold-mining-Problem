71b6fb557daef4f1f005cfe893eb3d505ec2a043
Malikoto <bigbig2540@hotmail.com>
2018-09-17 23:55:23 +0700

    Refactor Code

--- ShortestPathWithIterative.py
+++ ShortestPathWithIterative.py
@@ -3,196 +3,145 @@ import heapq
 from copy import deepcopy
 from collections import OrderedDict
 
-map = [[{'S':0},{'W':50},{'W':15},{'W':31},{'W':49},{'W':38},{'W':11},{'W':46},{'W':27},{'W':28},{'W':29},{'W':11},{'W':10},{'W':41},{'W':30},{'W':13}],
-[{'W':20},{'W':23},{'W':29},{'W':33},{'W':25},{'W':39},{'W':34},{'W':32},{'W':12},{'W':30},{'W':23},{'W':17},{'W':18},{'W':40},{'W':39},{'W':22}],
-[{'W':33},{'W':24},{'W':33},{'W':34},{'W':40},{'W':29},{'W':27},{'W':46},{'W':26},{'W':46},{'W':37},{'W':37},{'W':31},{'R':0},{'R':0},{'R':0}],
-[{'W':27},{'W':16},{'R':0},{'R':0},{'W':16},{'W':14},{'W':37},{'W':32},{'W':39},{'W':36},{'W':30},{'W':34},{'W':18},{'R':0},{'W':41},{'W':41}],
-[{'W':30},{'W':44},{'W':18},{'W':20},{'W':10},{'W':27},{'W':39},{'W':41},{'W':18},{'W':41},{'W':16},{'W':10},{'W':17},{'W':24},{'W':43},{'W':40}],
-[{'W':50},{'W':33},{'W':38},{'W':14},{'W':30},{'W':38},{'W':42},{'W':31},{'W':45},{'W':36},{'W':49},{'W':29},{'W':35},{'W':44},{'W':36},{'W':48}],
-[{'W':29},{'W':29},{'W':13},{'W':49},{'W':17},{'R':0},{'W':49},{'W':20},{'W':39},{'W':47},{'W':38},{'W':46},{'W':41},{'W':38},{'W':43},{'W':47}],
-[{'W':38},{'W':19},{'W':10},{'W':47},{'W':20},{'R':0},{'W':50},{'W':45},{'W':50},{'W':13},{'W':44},{'W':49},{'W':40},{'W':45},{'W':36},{'G':54237,'W':10}],
-[{'W':24},{'R':0},{'W':21},{'W':39},{'W':41},{'R':0},{'W':50},{'W':33},{'W':40},{'W':30},{'W':23},{'W':16},{'G':91159,'W':32},{'G':65034,'W':11},{'G':39582,'W':25},{'G':78932,'W':47}],
-[{'W':39},{'W':47},{'W':32},{'W':13},{'W':20},{'R':0},{'W':25},{'W':42},{'W':32},{'W':17},{'W':44},{'W':42},{'W':44},{'W':34},{'W':40},{'G':60745,'W':15}],
-[{'W':10},{'W':21},{'G':57517,'W':16},{'W':40},{'W':37},{'W':39},{'W':16},{'W':39},{'W':24},{'W':17},{'W':49},{'W':29},{'W':21},{'W':12},{'W':39},{'W':12}],
-[{'W':16},{'W':40},{'G':17908,'W':17},{'W':10},{'W':34},{'W':21},{'W':34},{'W':47},{'W':48},{'W':18},{'W':42},{'W':23},{'W':22},{'W':29},{'W':40},{'W':47}],
-[{'W':27},{'W':23},{'G':55470,'W':49},{'G':41849,'W':25},{'W':32},{'W':27},{'W':45},{'R':0},{'R':0},{'R':0},{'R':0},{'W':32},{'W':21},{'W':47},{'W':31},{'W':49}],
-[{'W':18},{'W':45},{'W':12},{'G':71088,'W':37},{'W':34},{'W':26},{'W':37},{'R':0},{'W':25},{'W':20},{'W':14},{'W':23},{'W':22},{'W':28},{'W':50},{'W':36}],
-[{'W':45},{'W':10},{'W':10},{'W':43},{'W':42},{'W':45},{'W':27},{'R':0},{'W':47},{'W':34},{'W':38},{'W':21},{'W':43},{'W':50},{'W':31},{'W':34}],
-[{'W':10},{'W':22},{'W':26},{'W':22},{'W':13},{'W':12},{'W':47},{'R':0},{'W':45},{'G':11984,'W':22},{'G':82595,'W':16},{'W':37},{'W':10},{'W':41},{'W':23},{'W':44}],
-[{'W':23},{'W':15},{'W':38},{'W':43},{'W':32},{'W':26},{'W':14},{'R':0},{'W':33},{'G':23042,'W':29},{'G':43840,'W':13},{'W':14},{'W':18},{'W':25},{'W':18},{'W':15}],
-[{'W':31},{'R':0},{'W':42},{'W':43},{'W':27},{'W':23},{'W':13},{'W':28},{'W':19},{'G':19174,'W':35},{'G':49269,'W':29},{'W':43},{'W':35},{'W':50},{'W':38},{'W':33}],
-[{'W':40},{'W':23},{'W':25},{'W':34},{'W':26},{'W':35},{'W':26},{'W':50},{'W':27},{'W':33},{'W':16},{'W':41},{'W':26},{'W':20},{'W':32},{'W':23}],
-[{'W':14},{'W':17},{'W':17},{'W':31},{'W':26},{'W':23},{'W':32},{'W':10},{'W':36},{'W':48},{'W':39},{'W':29},{'W':43},{'W':46},{'W':12},{'W':37}],
-[{'W':16},{'W':21},{'W':45},{'W':39},{'W':18},{'W':32},{'W':41},{'W':17},{'W':24},{'W':19},{'W':31},{'W':31},{'W':32},{'W':18},{'W':27},{'G':86461,'W':17}],
-[{'W':35},{'W':49},{'G':83730,'W':15},{'G':35960,'W':39},{'W':11},{'W':17},{'W':10},{'W':17},{'W':22},{'R':0},{'W':41},{'W':40},{'W':43},{'W':32},{'W':11},{'G':61646,'W':28}],
-[{'W':48},{'W':23},{'G':17416,'W':40},{'G':27403,'W':42},{'G':65923,'W':41},{'G':60978,'W':19},{'G':99450,'W':25},{'W':35},{'W':28},{'R':0},{'W':44},{'W':38},{'W':50},{'W':47},{'W':49},{'G':20390,'W':42}],
-[{'W':49},{'W':48},{'W':28},{'W':31},{'W':42},{'G':20103,'W':33},{'W':23},{'W':27},{'W':15},{'W':13},{'W':18},{'W':18},{'W':35},{'W':49},{'W':15},{'W':44}],
-[{'W':18},{'W':43},{'W':44},{'W':24},{'W':48},{'W':39},{'W':10},{'W':46},{'W':12},{'W':44},{'W':39},{'W':31},{'W':39},{'W':34},{'W':13},{'W':55}]]
+map = [[{'Start':0},{'Cost':50},{'Cost':15},{'Cost':31},{'Cost':49},{'Cost':38},{'Cost':11},{'Cost':46},{'Cost':27},{'Cost':28},{'Cost':29},{'Cost':11},{'Cost':10},{'Cost':41},{'Cost':30},{'Cost':13}],
+[{'Cost':20},{'Cost':23},{'Cost':29},{'Cost':33},{'Cost':25},{'Cost':39},{'Cost':34},{'Cost':32},{'Cost':12},{'Cost':30},{'Cost':23},{'Cost':17},{'Cost':18},{'Cost':40},{'Cost':39},{'Cost':22}],
+[{'Cost':33},{'Cost':24},{'Cost':33},{'Cost':34},{'Cost':40},{'Cost':29},{'Cost':27},{'Cost':46},{'Cost':26},{'Cost':46},{'Cost':37},{'Cost':37},{'Cost':31},{'Rock':0},{'Rock':0},{'Rock':0}],
+[{'Cost':27},{'Cost':16},{'Rock':0},{'Rock':0},{'Cost':16},{'Cost':14},{'Cost':37},{'Cost':32},{'Cost':39},{'Cost':36},{'Cost':30},{'Cost':34},{'Cost':18},{'Rock':0},{'Cost':41},{'Cost':41}],
+[{'Cost':30},{'Cost':44},{'Cost':18},{'Cost':20},{'Cost':10},{'Cost':27},{'Cost':39},{'Cost':41},{'Cost':18},{'Cost':41},{'Cost':16},{'Cost':10},{'Cost':17},{'Cost':24},{'Cost':43},{'Cost':40}],
+[{'Cost':50},{'Cost':33},{'Cost':38},{'Cost':14},{'Cost':30},{'Cost':38},{'Cost':42},{'Cost':31},{'Cost':45},{'Cost':36},{'Cost':49},{'Cost':29},{'Cost':35},{'Cost':44},{'Cost':36},{'Cost':48}],
+[{'Cost':29},{'Cost':29},{'Cost':13},{'Cost':49},{'Cost':17},{'Rock':0},{'Cost':49},{'Cost':20},{'Cost':39},{'Cost':47},{'Cost':38},{'Cost':46},{'Cost':41},{'Cost':38},{'Cost':43},{'Cost':47}],
+[{'Cost':38},{'Cost':19},{'Cost':10},{'Cost':47},{'Cost':20},{'Rock':0},{'Cost':50},{'Cost':45},{'Cost':50},{'Cost':13},{'Cost':44},{'Cost':49},{'Cost':40},{'Cost':45},{'Cost':36},{'Oil':54237,'Cost':10}],
+[{'Cost':24},{'Rock':0},{'Cost':21},{'Cost':39},{'Cost':41},{'Rock':0},{'Cost':50},{'Cost':33},{'Cost':40},{'Cost':30},{'Cost':23},{'Cost':16},{'Oil':91159,'Cost':32},{'Oil':65034,'Cost':11},{'Oil':39582,'Cost':25},{'Oil':78932,'Cost':47}],
+[{'Cost':39},{'Cost':47},{'Cost':32},{'Cost':13},{'Cost':20},{'Rock':0},{'Cost':25},{'Cost':42},{'Cost':32},{'Cost':17},{'Cost':44},{'Cost':42},{'Cost':44},{'Cost':34},{'Cost':40},{'Oil':60745,'Cost':15}],
+[{'Cost':10},{'Cost':21},{'Oil':57517,'Cost':16},{'Cost':40},{'Cost':37},{'Cost':39},{'Cost':16},{'Cost':39},{'Cost':24},{'Cost':17},{'Cost':49},{'Cost':29},{'Cost':21},{'Cost':12},{'Cost':39},{'Cost':12}],
+[{'Cost':16},{'Cost':40},{'Oil':17908,'Cost':17},{'Cost':10},{'Cost':34},{'Cost':21},{'Cost':34},{'Cost':47},{'Cost':48},{'Cost':18},{'Cost':42},{'Cost':23},{'Cost':22},{'Cost':29},{'Cost':40},{'Cost':47}],
+[{'Cost':27},{'Cost':23},{'Oil':55470,'Cost':49},{'Oil':41849,'Cost':25},{'Cost':32},{'Cost':27},{'Cost':45},{'Rock':0},{'Rock':0},{'Rock':0},{'Rock':0},{'Cost':32},{'Cost':21},{'Cost':47},{'Cost':31},{'Cost':49}],
+[{'Cost':18},{'Cost':45},{'Cost':12},{'Oil':71088,'Cost':37},{'Cost':34},{'Cost':26},{'Cost':37},{'Rock':0},{'Cost':25},{'Cost':20},{'Cost':14},{'Cost':23},{'Cost':22},{'Cost':28},{'Cost':50},{'Cost':36}],
+[{'Cost':45},{'Cost':10},{'Cost':10},{'Cost':43},{'Cost':42},{'Cost':45},{'Cost':27},{'Rock':0},{'Cost':47},{'Cost':34},{'Cost':38},{'Cost':21},{'Cost':43},{'Cost':50},{'Cost':31},{'Cost':34}],
+[{'Cost':10},{'Cost':22},{'Cost':26},{'Cost':22},{'Cost':13},{'Cost':12},{'Cost':47},{'Rock':0},{'Cost':45},{'Oil':11984,'Cost':22},{'Oil':82595,'Cost':16},{'Cost':37},{'Cost':10},{'Cost':41},{'Cost':23},{'Cost':44}],
+[{'Cost':23},{'Cost':15},{'Cost':38},{'Cost':43},{'Cost':32},{'Cost':26},{'Cost':14},{'Rock':0},{'Cost':33},{'Oil':23042,'Cost':29},{'Oil':43840,'Cost':13},{'Cost':14},{'Cost':18},{'Cost':25},{'Cost':18},{'Cost':15}],
+[{'Cost':31},{'Rock':0},{'Cost':42},{'Cost':43},{'Cost':27},{'Cost':23},{'Cost':13},{'Cost':28},{'Cost':19},{'Oil':19174,'Cost':35},{'Oil':49269,'Cost':29},{'Cost':43},{'Cost':35},{'Cost':50},{'Cost':38},{'Cost':33}],
+[{'Cost':40},{'Cost':23},{'Cost':25},{'Cost':34},{'Cost':26},{'Cost':35},{'Cost':26},{'Cost':50},{'Cost':27},{'Cost':33},{'Cost':16},{'Cost':41},{'Cost':26},{'Cost':20},{'Cost':32},{'Cost':23}],
+[{'Cost':14},{'Cost':17},{'Cost':17},{'Cost':31},{'Cost':26},{'Cost':23},{'Cost':32},{'Cost':10},{'Cost':36},{'Cost':48},{'Cost':39},{'Cost':29},{'Cost':43},{'Cost':46},{'Cost':12},{'Cost':37}],
+[{'Cost':16},{'Cost':21},{'Cost':45},{'Cost':39},{'Cost':18},{'Cost':32},{'Cost':41},{'Cost':17},{'Cost':24},{'Cost':19},{'Cost':31},{'Cost':31},{'Cost':32},{'Cost':18},{'Cost':27},{'Oil':86461,'Cost':17}],
+[{'Cost':35},{'Cost':49},{'Oil':83730,'Cost':15},{'Oil':35960,'Cost':39},{'Cost':11},{'Cost':17},{'Cost':10},{'Cost':17},{'Cost':22},{'Rock':0},{'Cost':41},{'Cost':40},{'Cost':43},{'Cost':32},{'Cost':11},{'Oil':61646,'Cost':28}],
+[{'Cost':48},{'Cost':23},{'Oil':17416,'Cost':40},{'Oil':27403,'Cost':42},{'Oil':65923,'Cost':41},{'Oil':60978,'Cost':19},{'Oil':99450,'Cost':25},{'Cost':35},{'Cost':28},{'Rock':0},{'Cost':44},{'Cost':38},{'Cost':50},{'Cost':47},{'Cost':49},{'Oil':20390,'Cost':42}],
+[{'Cost':49},{'Cost':48},{'Cost':28},{'Cost':31},{'Cost':42},{'Oil':20103,'Cost':33},{'Cost':23},{'Cost':27},{'Cost':15},{'Cost':13},{'Cost':18},{'Cost':18},{'Cost':35},{'Cost':49},{'Cost':15},{'Cost':44}],
+[{'Cost':18},{'Cost':43},{'Cost':44},{'Cost':24},{'Cost':48},{'Cost':39},{'Cost':10},{'Cost':46},{'Cost':12},{'Cost':44},{'Cost':39},{'Cost':31},{'Cost':39},{'Cost':34},{'Cost':13},{'Cost':55}]]
 
-START = (0,0)
 globalPath = []
-globalCost = []
 worthyMap = []
+
 path = []
-globalCost = 0
-allMine = []
+allOilWell = []
+
 colMax = len(map[0])
 rowMax = len(map)
 
-print(colMax)
-print(rowMax)
-
-
-
 def running(row, col, depth, depthLimit, copyMap):
-    print('depth' , depth)
 
-    if(depth == depthLimit):
-        return None
-
-    if col < 0:
-        return None
-    elif col >= colMax:
-        return None
-    if row < 0:
-        return None
-    elif row >= rowMax:
+    if depth == depthLimit:
         return None
 
-    if 'M' in copyMap[row][col]:
+    if col < 0 or col >= colMax or row < 0 or row >= rowMax:
         return None
-    copyMap[row][col]['M'] = 1
 
-    if 'G' in copyMap[row][col] and not 'GM' in copyMap[row][col]:
-        mine = []
-        findGold(row,col,mine)
+    if 'Mark' in copyMap[row][col]:
+        return None
+    copyMap[row][col]['Mark'] = 1
+
+    if 'Oil' in copyMap[row][col] and not 'OilMark' in copyMap[row][col]:
+        OilWell = []
+        findOil(row, col, OilWell)
+
         minCost = 999999
         minPath = []
         costInYield = 0
-        for gold in mine:
+
+        for OilYield in OilWell:
             newMap = deepcopy(copyMap)
-            x,y = gold['pos']
-            costInYield+=newMap[x][y]['W']
-            path , cost = findShortestPath(newMap,0,0,x,y)
+            x, y = OilYield['Pos']
+            costInYield += newMap[x][y]['Cost']
+            path, cost = findShortestPath(0, 0, newMap, x, y)
+
             if cost < minCost:
                 minCost = cost
                 minPath = path
-                costInYield -= newMap[x][y]['W']
+                costInYield -= newMap[x][y]['Cost']
 
         sumAllYield = 0
 
-        for i in mine:
-            sumAllYield += i['value']
+        for i in OilWell:
+            sumAllYield += i['Value']
+
         if(minCost!=999999):
-            worthyMap.append({'pathCost':minCost+costInYield, 'income':sumAllYield, 'profit':sumAllYield-(minCost+costInYield), 'path':minPath})
-            allMine.append(mine)
+            worthyMap.append({'PathCost':minCost+costInYield, 'Income':sumAllYield, 'Profit':sumAllYield-(minCost+costInYield), 'Path':minPath})
+            allOilWell.append(OilWell)
 
     running(row+1, col, depth+1, depthLimit, copyMap)
     running(row-1, col, depth+1, depthLimit, copyMap)
     running(row, col+1, depth+1, depthLimit, copyMap)
     running(row, col-1, depth+1, depthLimit, copyMap)
 
-def findClosetPoint(mine):
-    startPoint = (0,0)
-    minDistace = 999999
-    closetPoint = []
-    for m in mine:
-        dst = distance.euclidean( startPoint,( m['pos'][0], m['pos'][1] ) )
-        if minDistace > dst :
-            minDistace = dst
-            closetPoint = [m['pos'][0], m['pos'][1]]
-    return closetPoint
+def findOil(row, col, OilWell):
+    
+    if col < 0 or col >= colMax or row < 0 or row >= rowMax:
+        return None
 
-def findShortestPath(newMap,row,col,x,y):
+    if 'OilMark' in map[row][col]:
+        return None
+    
+    if not ('Oil' in map[row][col]):
+        return None
+    else:
+        OilWell.append({'Value': map[row][col]['Oil'], 'Pos': [row, col]})
+    map[row][col]['OilMark'] = 1
 
+    findOil(row + 1, col, OilWell)
+    findOil(row - 1, col, OilWell)
+    findOil(row, col + 1, OilWell)
+    findOil(row, col - 1, OilWell)
 
-    # if 'SM' in newMap[row][col]:
-    #     return None
-    # newMap[row][col]['SM'] = 1
+def findShortestPath(row,col,newMap,x,y):
 
-    path,cost = ucs((row,col), (x,y), newMap)
-    return path,cost
-
-    '''
-    directD = {}
-    directD['up'] = {'row': row-1, 'col': col}
-    directD['down'] = {'row': row+1, 'col': col}
-    directD['left'] = {'row': row, 'col': col-1}
-    directD['right'] = {'row': row, 'col': col+1}
-
-    coordinate = {}
-
-    coordinate['up'] = newMap[row-1][col]['W'] if row-1 > 0 and not 'SM' in newMap[row-1][col] and not 'R' in newMap[row-1][col] else 9999
-    coordinate['down'] = newMap[row+1][col]['W'] if row+1 > 0 and not 'SM' in newMap[row+1][col] and not 'R' in newMap[row+1][col] else 9999
-    coordinate['left'] = newMap[row][col-1]['W'] if col-1 > 0 and not 'SM' in newMap[row][col-1] and not 'R' in newMap[row][col-1] else 9999
-    coordinate['right'] = newMap[row][col+1]['W'] if col+1 > 0 and not 'SM' in newMap[row][col+1] and not 'R' in newMap[row][col+1] else 9999
-    sort = OrderedDict(sorted(coordinate.items(), key=lambda t: t[1]))
-    a,b,c,d = sort.keys()
-    print(sort,a,b,c,d)
-    choose = min(coordinate, key=coordinate.get)
-    Ncol,Nrow = directD[choose].values()
-    Ncost = coordinate[choose]
-    #print(Nrow,Ncol)
-    lastPath.append([Nrow,Ncol])
-    print(lastPath)
-    print(Ncost+cost)
-    findShortestPath(newMap,Nrow,Ncol,mine,lastPath,cost+Ncost)
-    '''
-    #
-    #
-    # if min(up,down,left,right) == up:
-    #     findShortestPath()
-    #
-    # lastPath.append([row,col])
-    #
-    # if(min(map[row+1][col]))
-
+    path, cost = ucs((row,col), (x,y), newMap)
+    return path, cost
 
 def children(point, newMap):
+    
     row, col = point
 
     coordinate = {}
 
-    coordinate['up'] = newMap[row-1][col]['W'] if row-1 >= 0 and not 'SM' in newMap[row-1][col] and not 'R' in newMap[row-1][col] and not 'S' in newMap[row-1][col] else 9999
-    coordinate['down'] = newMap[row+1][col]['W'] if row+1 < rowMax and not 'SM' in newMap[row+1][col] and not 'R' in newMap[row+1][col] and not 'S' in newMap[row+1][col] else 9999
-    coordinate['left'] = newMap[row][col-1]['W'] if col-1 >= 0 and not 'SM' in newMap[row][col-1] and not 'R' in newMap[row][col-1] and not 'S' in newMap[row][col-1] else 9999
-    coordinate['right'] = newMap[row][col+1]['W'] if col+1 < colMax and not 'SM' in newMap[row][col+1] and not 'R' in newMap[row][col+1] and not 'S' in newMap[row][col+1] else 9999
+    coordinate['Up']    = newMap[row-1][col]['Cost'] if row-1 >= 0     and not 'SM' in newMap[row-1][col] and not 'Rock' in newMap[row-1][col] and not 'Start' in newMap[row-1][col] else 9999
+    coordinate['Down']  = newMap[row+1][col]['Cost'] if row+1 < rowMax and not 'SM' in newMap[row+1][col] and not 'Rock' in newMap[row+1][col] and not 'Start' in newMap[row+1][col] else 9999
+    coordinate['Left']  = newMap[row][col-1]['Cost'] if col-1 >= 0     and not 'SM' in newMap[row][col-1] and not 'Rock' in newMap[row][col-1] and not 'Start' in newMap[row][col-1] else 9999
+    coordinate['Right'] = newMap[row][col+1]['Cost'] if col+1 < colMax and not 'SM' in newMap[row][col+1] and not 'Rock' in newMap[row][col+1] and not 'Start' in newMap[row][col+1] else 9999
 
-    children = [(row - 1, col), (row + 1, col),(row, col - 1), (row, col + 1) ]
-    #print(coordinate)
+    children = [(row - 1, col), (row + 1, col),(row, col - 1), (row, col + 1)]
+
     direction = []
+    
     for key,val in coordinate.items():
-        if(key == 'up' and val != 9999):
+        if(key == 'Up' and val != 9999):
             direction.append(children[0])
-        elif(key == 'down' and val != 9999):
+        elif(key == 'Down' and val != 9999):
             direction.append(children[1])
-        elif(key == 'left' and val != 9999):
+        elif(key == 'Left' and val != 9999):
             direction.append(children[2])
-        elif (key == 'right' and val != 9999):
+        elif (key == 'Right' and val != 9999):
             direction.append(children[3])
-    #print(direction)
     return direction
 
-    #
-    # for child in children:
-    #     for key,val in coordinate:
-    #         for
-    #         print('KEY ' ,key ,val)
-    #return [child for child in children if grid[child[0]][child[1]] != '%']
-
-    return coordinate
-
-
-
-def ucs(node, goal, grid):
+def ucs(node, goal, map):
     # Initialize the queue with the root node
-    q = [(0, node, [])]
+    queue = [(0, node, [])]
     # The list of seen items
     seen = {}
     # While the queue isn't empty
-    while q:
+    while queue:
         # Pop the cost, point and path from the queue
-        cost, point, path = heapq.heappop(q)
+        cost, point, path = heapq.heappop(queue)
         # If it has been seen, and has a lower cost, bail
         if point in seen and seen[point] < cost:
             continue
@@ -202,62 +151,33 @@ def ucs(node, goal, grid):
         if point == goal:
             return path,cost
         # Loop through the children
-        for child in children(point, grid):
+        for child in children(point, map):
             # Calculate the basic cost
-            x,y = child
-            child_cost = grid[x][y]['W']
+            x, y = child
+            child_cost = map[x][y]['Cost']
             # If the child hasn't been seen
             if child not in seen:
                 # Add it to the heap
-                heapq.heappush(q, (cost + child_cost, child, path))
+                heapq.heappush(queue, (cost + child_cost, child, path))
         # Add the point to the seen items
         seen[point] = cost
     return None
-
-def findGold(row, col, mine):
-    if col < 0:
-        return None
-    elif col >= colMax:
-        return None
-    if row < 0:
-        return None
-    elif row >= rowMax:
-        return None
-
-    if 'GM' in map[row][col]:
-        return None
-    if not('G' in map[row][col]):
-        return None
-    else:
-        mine.append({'value':map[row][col]['G'], 'pos':[row, col]})
-    map[row][col]['GM'] = 1
-
-    findGold(row + 1, col, mine)
-    findGold(row - 1, col, mine)
-    findGold(row, col + 1, mine)
-    findGold(row, col - 1, mine)
-
 
 depthLimit = 400
 i = 1
 
 for depth in range(0,depthLimit):
-
     copyMap = []
     copyMap = deepcopy(map)
     running(0,0,0,i,copyMap)
     i+=1
-    print(depth)
-#
-# copyMap = map[:]
-# running(0,0,0,15,copyMap)
 
-maxGoldMine = 0
+maxOilWell = 0
 winMap = {}
 
 for map in worthyMap:
-    print(map)
-    if(map['profit']>maxGoldMine):
-        maxGoldMine = map['profit']
+    if(map['Profit']>maxOilWell):
+        maxOilWell = map['Profit']
         winMap = map
+        
 print('Winner = ' , winMap)
